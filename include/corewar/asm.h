d7e54ad91391448d3dce27741f55b2bd5860808b
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2023-05-05 11:22:20 +0200

    Added asm definitions

--- include/corewar/asm.h
+++ include/corewar/asm.h
@@ -8,4 +8,12 @@
 #ifndef COREWAR_ASM_H
     #define COREWAR_ASM_H
 
+    #include "op.h"
+
+typedef struct precoded_op_s {
+    op_code_t code;
+    arg_type_t arg_types[MAX_ARGS_NUMBER];
+    uint64_t args[MAX_ARGS_NUMBER];
+} precoded_op_t;
+
 #endif /* !COREWAR_ASM_H */
--- include/corewar/op.h
+++ include/corewar/op.h
@@ -38,11 +38,31 @@
 
 typedef unsigned char arg_type_t;
 
+typedef enum op_code_e {
+    OP_NONE = 0,
+    OP_LIVE = 1,
+    OP_LD = 2,
+    OP_ST = 3,
+    OP_ADD = 4,
+    OP_SUB = 5,
+    OP_AND = 6,
+    OP_OR = 7,
+    OP_XOR = 8,
+    OP_ZJMP = 9,
+    OP_LDI = 10,
+    OP_STI = 11,
+    OP_FORK = 12,
+    OP_LLD = 13,
+    OP_LLDI = 14,
+    OP_LFORK = 15,
+    OP_AFF = 16
+} op_code_t;
+
 typedef struct op_s {
+    op_code_t code;
     char mnemonic[8];
     uint8_t nbr_args;
     arg_type_t type[MAX_ARGS_NUMBER];
-    uint8_t code;
     uint16_t nbr_cycles;
 } op_t;
 
@@ -52,59 +72,60 @@ typedef struct op_s {
 
 static const op_t op_tab[21] = {
     /*
+    ** Code,
     ** Mnemonic,
     ** Number of arguments, {
     **   Type of argument[,
     **   Type of argument...]
     ** },
-    ** Code,
     ** Number of cycles
     */
     {
+        OP_LIVE,
         "live", // Live
         1, {T_DIR}, // live <player number>
-        1,
         10
     },
     {
+        OP_LD,
         "ld",   // Load memory into register
         2, {
             T_DIR | T_IND,
             T_REG
         },          // ld <val|addr>,<reg>
-        2,
         5
     },
     {
+        OP_ST,
         "st",   // Copy register value into memory/register
         2, {
             T_REG,
             T_IND | T_REG
         },          // st <reg>,<addr|reg>
-        3,
         5
     },
     {
+        OP_ADD,
         "add",  // Sum two registers and store the result in a register
         3, {
             T_REG,
             T_REG,
             T_REG
         },          // add <reg>,<reg>,<reg>
-        4,
         10
     },
     {
+        OP_SUB,
         "sub",  // Subtract two registers and store the result in a register
         3, {
             T_REG,
             T_REG,
             T_REG
         },          // sub <reg>,<reg>,<reg>
-        5,
         10
     },
     {
+        OP_AND,
         "and",  // Bitwise AND of two register/value/memory at address
                 // and store the result in a register
         3, {
@@ -112,10 +133,10 @@ static const op_t op_tab[21] = {
             T_REG | T_DIR | T_IND,
             T_REG
         },          // and <reg|val|addr>,<reg|val|addr>,<reg>
-        6,
         6
     },
     {
+        OP_OR,
         "or",   // Bitwise OR of two register/value/memory at address
                 // and store the result in a register
         3, {
@@ -123,10 +144,10 @@ static const op_t op_tab[21] = {
             T_REG | T_IND | T_DIR,
             T_REG
         },          // or <reg|val|addr>,<reg|val|addr>,<reg>
-        7,
         6
     },
     {
+        OP_XOR,
         "xor",  // Bitwise XOR of two register/value/memory at address
                 // and store the result in a register
         3, {
@@ -134,70 +155,69 @@ static const op_t op_tab[21] = {
             T_REG | T_IND | T_DIR,
             T_REG
         },          // xor <reg|val|addr>,<reg|val|addr>,<reg>
-        8,
         6
     },
     {
+        OP_ZJMP,
         "zjmp", // Jump to address (set PC to value)
         1, {T_DIR}, // zjmp <val>
-        9,
         20
     },
     {
+        OP_LDI,
         "ldi",
         3, {
             T_REG | T_DIR | T_IND,
             T_DIR | T_REG,
             T_REG
         },
-        10,
         25
     },
     {
+        OP_STI,
         "sti",
         3, {
             T_REG,
             T_REG | T_DIR | T_IND,
             T_DIR | T_REG
         },
-        11,
         25
     },
     {
+        OP_FORK,
         "fork",
         1, {T_DIR},
-        12,
         800
     },
     {
+        OP_LLD,
         "lld",
         2, {
             T_DIR | T_IND,
             T_REG
         },
-        13,
         10
     },
     {
+        OP_LLDI,
         "lldi",
         3, {
             T_REG | T_DIR | T_IND,
             T_DIR | T_REG,
             T_REG
         },
-        14,
         50
     },
     {
+        OP_LFORK,
         "lfork",
         1, {T_DIR},
-        15,
         1000
     },
     {
+        OP_AFF,
         "aff",
         1, {T_REG},
-        16,
         2
     }
 };
